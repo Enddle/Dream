8a820239eba6038f62e1be47d634e00a2915dd01
Enddle <support@enddle.me>
2023-01-15 03:26:56 +0900

    fix lightning gap

--- Scenes/Spaces/LightningControl.gd
+++ Scenes/Spaces/LightningControl.gd
@@ -14,7 +14,7 @@ func _input(event):
 	if event is InputEventMouseButton and !event.is_pressed():
 #		print("Mouse Unclick at: ", event.position)
 		if OS.get_ticks_msec() > nextThunder:
-			nextThunder = OS.get_ticks_msec() + 350
+			nextThunder = OS.get_ticks_msec() + 500
 #			print("Thunder at: ", event.position.x / 2266)
 			
 			TA.touch(event.position)
